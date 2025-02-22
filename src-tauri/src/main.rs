c61ff889bf2cd09300cdabfb505bce1188ecaf9b
Miha <stele1000@gmail.com>
2025-02-22 18:44:01 +0100

    Small tweaks

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// fn main() {
-//     db_client_lib::run()
-// }
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use serde::{Deserialize, Serialize};
 use sqlx::{Column, MySqlPool, PgPool, Row};
 
